0541f4291797bf683bd7f6d5bbdf54d40bef3548
Anmol Ahuja <ahuja.anmol03@gmail.com>
2021-12-12 00:08:21 +0530

    (update) : dashboard

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const exphbs = require("express-handlebars");
-const passport = require("passport");
-const session = require("express-session");
-const connectDB = require("./config/db.js");
+const path = require("path")
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const morgan = require("morgan")
+const exphbs = require("express-handlebars")
+const passport = require("passport")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")(session)
+const connectDB = require("./config/db")
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
@@ -13,18 +15,18 @@ dotenv.config({ path: "./config/config.env" });
 // Passport config
 require("./config/passport")(passport);
 
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
 // Logging
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  app.use(morgan("dev"))
 }
 
 // Handlebars
 app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
-app.set("view engine", ".hbs");
+app.set("view engine", ".hbs")
 
 // Sessions
 app.use(
@@ -32,6 +34,7 @@ app.use(
     secret: "hello",
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
   })
 );
 
