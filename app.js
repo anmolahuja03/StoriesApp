9cdbcc8b70675826e802f208f1e072702558d03b
Anmol Ahuja <ahuja.anmol03@gmail.com>
2021-12-12 01:34:16 +0530

    (update) : public stories

--- app.js
+++ app.js
@@ -20,16 +20,28 @@ connectDB();
 const app = express();
 
 // Body parser
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Handlbars helpers
+const { formatDate } = require("./helpers/hbs");
+
 // Handlebars
-app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    helpers: {
+      formatDate,
+    },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 app.set("view engine", ".hbs");
 
 // Sessions
